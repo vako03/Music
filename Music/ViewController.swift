5b4a62111ef2288e9936ab4d64c2caeae2074249
Vako <valeri.mekhashishvili97@gmail.com>
2024-05-10 02:39:58 +0400

    initial commit

--- Music/ViewController.swift
+++ Music/ViewController.swift
@@ -8,12 +8,99 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+      private let coverImage: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = UIImage(named: "swift")
+        return image
+    }()
 
+    let containerView = UIView()
+    let soLongLabel = UILabel(text: "So Long, London", fontSize: 24, color: .white)
+    let taylorSwiftLabel = UILabel(text: "Taylor Swift", fontSize: 16, color: .gray)
+
+    // Add progress bar
+    private let progressBar: UIProgressView = {
+        let progressBar = UIProgressView(progressViewStyle: .default)
+        progressBar.translatesAutoresizingMaskIntoConstraints = false
+        progressBar.progressTintColor = .blue // Adjust color as needed
+        progressBar.progress = 0.5 // Set initial progress value
+        return progressBar
+    }()
+    let backgroundColor = UIColor(red: 22/255, green: 20/255, blue: 17/255, alpha: 1.0)
+
+    let stackView = UIStackView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = backgroundColor
+        setupUI()
+        addButtonsToStack()
+
+    }
+    
+   func setupUI() {
+       
+       view.addSubview(coverImage)
+       view.addSubview(soLongLabel)
+       view.addSubview(taylorSwiftLabel)
+       view.addSubview(progressBar)
+       containerView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(containerView)
+
+       stackView.axis = .horizontal // Set axis to horizontal
+       stackView.spacing = 20
+       stackView.translatesAutoresizingMaskIntoConstraints = false
+       view.addSubview(stackView)
+       stackView.backgroundColor = nil
+       
+       
+       NSLayoutConstraint.activate([
+        coverImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
+        coverImage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 35),
+        coverImage.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -35),
+        
+        containerView.topAnchor.constraint(equalTo: coverImage.bottomAnchor, constant: 34),
+        containerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        
+        soLongLabel.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+        soLongLabel.topAnchor.constraint(equalTo: containerView.topAnchor),
+        
+        taylorSwiftLabel.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+        taylorSwiftLabel.topAnchor.constraint(equalTo: soLongLabel.bottomAnchor, constant: 7),
+        taylorSwiftLabel.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
+        
+        progressBar.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        progressBar.topAnchor.constraint(equalTo: taylorSwiftLabel.bottomAnchor, constant: 34),
+        progressBar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 21),
+        progressBar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -21),
+                stackView.topAnchor.constraint(equalTo: progressBar.bottomAnchor, constant: 34),
+                stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+                stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+            ])
+     
+        
+        
     }
 
+    func addButtonsToStack() {
+        let buttonIcons: [UIImage] = [
+            UIImage(named: "shuffle")!,
+            UIImage(named: "skip-back")!,
+            UIImage(named: "playButton")!,
+            UIImage(named: "skip-forward")!,
+            UIImage(named: "repeat")!
+        ]
+        
+        for icon in buttonIcons {
+            let button = UIButton.createCustomButton(icon: icon)
+            stackView.addArrangedSubview(button)
+        }
+    }
 
 }
 
+#Preview {
+    ViewController()
+}
---
+++ Music/extension.swift
@@ -0,0 +1,35 @@
+//
+//  extension.swift
+//  Music
+//
+//  Created by valeri mekhashishvili on 10.05.24.
+//
+
+import Foundation
+import UIKit
+
+
+extension UILabel {
+    convenience init(text: String, fontSize: CGFloat, color: UIColor) {
+        self.init()
+        self.text = text
+        self.font = UIFont.systemFont(ofSize: fontSize)
+        self.textColor = color
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
+
+extension UIButton {
+    static func createCustomButton(icon: UIImage) -> UIButton {
+        let button = UIButton()
+        button.setTitleColor(.white, for: .normal)
+        button.setImage(icon, for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
+        button.backgroundColor = nil
+        button.layer.cornerRadius = 8
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        return button
+    }
+}
+
